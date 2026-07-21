374aee949c0de809b3a189ef59cf998f03fe9f18
Aivo88 <antti.kaariainen88@gmail.com>
2026-07-21 22:16:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SE7EN V2 service worker
 // Bump CACHE on every release.
-const CACHE = 'se7env2-v23';
+const CACHE = 'se7env2-v24';
 const ASSETS = ['./', './index.html', './manifest.json', './header-default.png', './icon-192.png', './icon-512.png'];
 
 // cache:'reload' is essential: without it the browser may serve these files
