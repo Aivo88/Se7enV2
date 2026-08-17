30017485c30be8eaac3062c0f48d011946884ff2
Aivo88 <antti.kaariainen88@gmail.com>
2026-08-17 14:36:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SE7EN V2 service worker
 // Bump CACHE on every release.
-const CACHE = 'se7env2-v52';
+const CACHE = 'se7env2-v53';
 const ASSETS = ['./', './index.html', './manifest.json', './header-default.png',
                 './icon-192.png', './icon-512.png'];
 
